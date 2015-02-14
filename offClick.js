9acf48dec8bcc55dd000006b94db5bd6e2fe4a64
Evan Sharp <dumbdrum@gmail.com>
2015-02-14 18:41:13 -0500

    Chore(cleanup): Made more like angular's events
    See https://github.com/angular/angular.js/blob/g3_v1_2/src/ng/directive/ngEventDirs.js#L54 for how angular handles events

--- offClick.js
+++ offClick.js
@@ -1,57 +1,55 @@
-angular.module('offClick',[])
-.directive('offClick', ['$document', '$timeout', function ($document, $timeout) {
+angular.module('offClick', [])
+    .directive('offClick', ['$document', '$rootScope', '$timeout', '$parse', function ($document, $rootScope, $timeout, $parse) {
 
-    function targetInFilter(target,filter){
-        if(!target || !filter) return false;
+    function targetInFilter(target, filter) {
+        if (!target || !filter) return false;
         var elms = angular.element(document.querySelectorAll(filter));
         var elmsLen = elms.length;
-        for (var i = 0; i< elmsLen; ++i)
-            if(elms[i].contains(target)) return true;
+        for (var i = 0; i < elmsLen; ++i)
+        if (elms[i].contains(target)) return true;
         return false;
     }
 
     return {
         restrict: 'A',
-        scope: {
-            offClick: '&',
-            offClickIf: '&'
-        },
-        link: function (scope, elm, attr) {
-
-            if (attr.offClickIf) {
-                scope.$watch(scope.offClickIf, function (newVal, oldVal) {
+        compile: function ($element, attr) {
+            var fn = $parse(attr.offClick);
+            return function (scope, element) {
+                var offClickFilter;
+                if (attr.offClickIf) {
+                    $rootScope.$watch(attr.offClickIf, function (newVal, oldVal) {
                         if (newVal && !oldVal) {
-                            $timeout(function() {
+                            $timeout(function () {
                                 $document.on('click', handler);
                             });
-                        } else if(!newVal){
+                        } else if (!newVal) {
                             $document.off('click', handler);
                         }
-                    }
-                );
-            } else {
-                $document.on('click', handler);
-            }
-
-            scope.$on('$destroy', function() {
-                $document.off('click', handler);
-            });
-
-            var offClickFilter;
-            attr.$observe('offClickFilter', function(value) {
-                offClickFilter = value;
-            });
-
-            function handler(event) {
-                // This filters out artificial click events. Example: If you hit enter on a form to submit it, an
-                // artificial click event gets triggered on the form's submit button.
-                if (event.pageX == 0 && event.pageY == 0) return;
-
-                var target = event.target || event.srcElement;
-                if (!(elm[0].contains(target) || targetInFilter(target, offClickFilter))) {
-                    scope.$apply(scope.offClick());
+                    });
+                } else {
+                    $document.on('click', handler);
                 }
-            }
+
+                attr.$observe('offClickFilter', function (value) {
+                    offClickFilter = value;
+                });
+
+                function handler(event) {
+                    // This filters out artificial click events. Example: If you hit enter on a form to submit it, an
+                    // artificial click event gets triggered on the form's submit button.
+                    if (event.pageX == 0 && event.pageY == 0) return;
+
+                    var target = event.target || event.srcElement;
+                    if (!(element[0].contains(target) || targetInFilter(target, offClickFilter))) {
+                        var callback = function () {
+                            fn(scope, {
+                                $event: event
+                            });
+                        };
+                        scope.$apply(callback);
+                    }
+                }
+            };
         }
     };
 }]);
